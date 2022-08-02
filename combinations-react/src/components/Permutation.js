1742856d93e46e615de8bf48e556be115e70f23b
LobitoARG <hjabo97@gmail.com>
2022-08-02 00:26:34 -0300

    subo la nueva version a mi branch

---
+++ combinations-react/src/components/Permutation.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Permutation() {
+  return (
+    <div className='bg-green-200'>
+    <div className='flex justify-around mt-5'>
+      <div>
+        <h2 className='text-center'>Total number</h2>
+        <input typeof='number'></input>
+      </div>
+      <div>
+        <h2 className='text-center'>Group number</h2>
+        <input typeof='number'></input>
+      </div>
+    </div>
+    <div className='text-center mt-5'>
+      <button className='px-5 py-2'>Calculate</button>
+    </div>
+    <div className='border-solid pb-5 w-screen mt-5'>
+      <h2 className='text-center'>Result: 200</h2>
+    </div>
+  </div>
+  )
+}
