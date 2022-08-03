904e6353b3b41ce470828e41696bca906c5f825a
saravalentinal <latynsara41@gmail.com>
2022-08-04 01:24:42 +0200

    css main version 2

--- combinations-react/src/components/Variation.js
+++ combinations-react/src/components/Variation.js
@@ -1,10 +1,44 @@
 import React from 'react'
+import { useState } from 'react';
 
 export default function Variation() {
+
+
+  const [result, setResult] = useState(['']);
+
+  const submitHandler = e =>{
+    e.preventDefault();
+    const totalNumber = parseInt(e.target.total.value,10);
+    const groupNumber = parseInt(e.target.total2.value, 10);
+    
+    function recursiveFactorial(numberToFactorial){
+      if (numberToFactorial === 0)
+      {
+        return 1;
+      }
+
+      return numberToFactorial * recursiveFactorial(numberToFactorial-1)
+    }
+
+    function variationFormula(){
+      const result = recursiveFactorial(totalNumber) / recursiveFactorial(totalNumber - groupNumber);
+      return result
+    }
+
+    setResult(variationFormula());
+
+    e.target.total.value = '';
+    e.target.total2.value = '';
+  }
+
+
+
+
+
   return (
     <>
     <div className='calc-box'>
-      <form>
+      <form onSubmit={submitHandler}>
   
         <div className='numbers'>
   
@@ -28,7 +62,7 @@ export default function Variation() {
     </div>
   
     <div className='div-result'>
-          <h2 className='result'>Result: </h2>
+          <h2 className='result'>Result: {result} </h2>
     </div>
     
     </>
