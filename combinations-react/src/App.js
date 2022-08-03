eaf096709228afa9e0c5aa2b48b673f3f411b1bb
saravalentinal <latynsara41@gmail.com>
2022-08-03 14:12:23 +0200

    Permutation Component now works

--- combinations-react/src/App.js
+++ combinations-react/src/App.js
@@ -1,30 +1,30 @@
 import Main from './components/Main'
 import './index.css'
-//import Questions from './components/Questions'
+import Questions from './components/Questions'
 
 function App() {
   return (
   <>
     <Main/>
-   {/*  <section className='questions-section'>
+    <section className='questions-section'>
           <h1 className='title-questions'>Combinatorics have never been easier</h1>
-          <Questions
+          < Questions
           question = "What is Combinatorics?"
           answer = "Combinatorics is an area of mathematics primarily concerned with counting, both as a means and an end in obtaining results, and certain properties of finite structures. It is closely related to many other areas of mathematics and has many applications ranging from logic to statistical physics and from evolutionary biology to computer science."
           />
-          <Questions
+          < Questions
           question = "What is Combination?"
           answer = "A combination is a mathematical technique that determines the number of possible arrangements in a collection of items where the order of the selection does not matter. In combinations, you can select the items in any order."
           />
-          <Questions 
+          < Questions 
           question = "What is Permutation?"
           answer = "A permutation is a mathematical calculation of the number of ways a particular set can be arranged, where the order of the arrangement matters."
           />
-          <Questions 
+          < Questions 
           question = "What is Variation?"
           answer = "Variations are arrangements of selections of objects with or without repetition, where the order of the selected objects matters."
           />
-    </section> */}
+      </section>
  </>
   );
 }
--- combinations-react/src/components/Permutation.js
+++ combinations-react/src/components/Permutation.js
@@ -1,48 +1,51 @@
 import React from 'react'
+import { useState } from 'react';
 
 export default function Permutation() 
 {
 
+  const [result, setResult] = useState(['']);
+
   const submitHandler = e =>{
     e.preventDefault();
-    const numero = parseInt(e.target.total.value,10);
-    const numero2 = parseInt(e.target.total2.value, 10);
+    const totalNumber = parseInt(e.target.total.value,10);
+    const groupNumber = parseInt(e.target.total2.value, 10);
     
-    function factorialRecursivo(n){
-      if (n === 0)
+    function recursiveFactorial(numberToFactorial){
+      if (numberToFactorial === 0)
       {
         return 1;
       }
 
-      return n * factorialRecursivo(n-1)
+      return numberToFactorial * recursiveFactorial(numberToFactorial-1)
     }
 
-    function permutacion(){
-      const resultado = factorialRecursivo(numero) / factorialRecursivo(numero - numero2);
-      return resultado
+    function permutationFormula(){
+      const result = recursiveFactorial(totalNumber) / recursiveFactorial(totalNumber - groupNumber);
+      return result
     }
 
-    console.log(permutacion())
-    
+    setResult(permutationFormula());
+
+    e.target.total.value = '';
+    e.target.total2.value = '';
   }
   
-
-
   return (
     <>
     <div className='bg-green-200'>
     <div className='flex justify-around mt-5'>
       <form onSubmit={submitHandler}>
-      <h2 className='text-center'>Total number</h2>
+      <h2 className='text-center'>Total number (n)</h2>
       <input type='number' name='total'></input>
 
-      <h2 className='text-center'>Group number</h2>
+      <h2 className='text-center'>Group number (r)</h2>
       <input type='number' name='total2'></input>
       <button className='px-5 py-2' type='submit'>Calculate</button>
       </form>
 
     <div className='border-solid pb-5 w-screen mt-5'>
-      <h2 className='text-center'>Result: 200</h2>
+      <h2 className='text-center'>Result:{result}</h2>
     </div>
     </div>
     </div>
