1031f8154695e58a5f5c86c2ac888c70c37074a0
saravalentinal <latynsara41@gmail.com>
2022-08-07 18:28:40 +0200

    integrating language and dm

--- combinations-react/src/App.js
+++ combinations-react/src/App.js
@@ -1,15 +1,17 @@
-//import Main from './components/Main'
 import './index.css'
 import Questions from './components/Questions'
 import Footer from './components/Footer'
 import { createContext, useState} from 'react'
 import Main from './components/Main'
+import { useTranslation } from 'react-i18next'
 
 
 
 export const ThemeContext = createContext(null)
 
 function App() {
+
+  const { t, i18n } = useTranslation();
 
   const [theme, setTheme] = useState("light");
 
@@ -21,6 +23,14 @@ function App() {
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
   <>
     <div className='main-app' id={theme}>
+    
+    <div>
+          {Object.keys(lngs).map((lng) => (
+            <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
+              {lngs[lng].nativeName}
+            </button>
+          ))}
+        </div>
 
     <div className = 'main-div'>
       <div className='nav-div'>
@@ -35,22 +45,22 @@ function App() {
     </div>
     
     <section className='questions-section'>
-          <h1 className='title-questions' id="question-title">Combinatorics have never been easier</h1>
+          <h1 className='title-questions' id="question-title">{t('questions.title')}</h1>
           < Questions
-          question = "What is Combinatorics?"
-          answer = "Combinatorics is an area of mathematics primarily concerned with counting, both as a means and an end in obtaining results, and certain properties of finite structures. It is closely related to many other areas of mathematics and has many applications ranging from logic to statistical physics and from evolutionary biology to computer science."
+          question = {t('questions.first')}
+          answer = {t('answers.first')}
           />
           < Questions
-          question = "What is Combination?"
-          answer = "A combination is a mathematical technique that determines the number of possible arrangements in a collection of items where the order of the selection does not matter. In combinations, you can select the items in any order."
+          question = {t('questions.second')}
+          answer = {t('answers.second')}
           />
           < Questions 
-          question = "What is Permutation?"
-          answer = "A permutation is a mathematical calculation of the number of ways a particular set can be arranged, where the order of the arrangement matters."
+          question = {t('questions.third')}
+          answer = {t('answers.third')}
           />
           < Questions 
-          question = "What is Variation?"
-          answer = "Variations are arrangements of selections of objects with or without repetition, where the order of the selected objects matters."
+          question = {t('questions.forth')}
+          answer = {t('answers.forth')}
           />
       </section>
 
