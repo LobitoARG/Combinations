5ab75b219eb765e03e2599f036628619f2cbd906
saravalentinal <latynsara41@gmail.com>
2022-08-19 15:41:52 +0200

    responsive design added in main

--- combinations-react/src/components/Options.js
+++ combinations-react/src/components/Options.js
@@ -8,9 +8,18 @@ export default function Options() {
   return (
     <>
     <div className="options">
+      <div>
         <NavLink to='/combination' exact><button id="button-comb">{t('options.one')}</button></NavLink>
+      </div>
+
+      <div>
         <NavLink to='/permutation' exact><button>{t('options.two')}</button></NavLink>
+      </div>
+
+      <div>
         <NavLink to='variation' exact><button>{t('options.three')}</button></NavLink>
+      </div>
+        
     </div>
     </>
   )
